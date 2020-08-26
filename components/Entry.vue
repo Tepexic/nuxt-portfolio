b9164c71aaafa8de169368016e11f418b6a4b689
Jesus Velazquez <jvelaz46@ford.com>
2020-08-26 10:42:52 -0500

    Add new project - Thesis template

--- components/Entry.vue
+++ components/Entry.vue
@@ -15,7 +15,7 @@
       <p class="text-base">{{ description }}</p>
     </div>
     <!--author/date-->
-    <div class="flex items-center  mb-2 justify-end">
+    <div class="flex items-center mb-2 justify-end">
         <div class="flex items-center">
             <svg class="h-3 w-3 fill-current mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
             <div class="mt-1 text-right text-xs mr-4">
